efc26f266bc3ace8d5ca049cfa88ec73e3c74c15
alevani <vanini.alexandre@gmail.com>
2020-09-22 18:12:52 +0200

    Add planetary support for gravito
    Creates a planet in the middle + if user presses the mousepad a new planet pops up in the canva

--- gravito/js/sketch.js
+++ gravito/js/sketch.js
@@ -1,12 +1,47 @@
+let InterstellarObjects = [];
+
+let InterstellarObject = function (m, x, y) {
+    this.mass = m;
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+};
+
+InterstellarObject.prototype.display = function () {
+    stroke(0);
+    strokeWeight(2);
+    fill("#2fa5f5");
+    ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
+}
+
+
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    // Set safe the sun in the center
+    background("#0c1f36");
+    ellipse(window.innerWidth / 2, window.innerHeight / 2, 80, 80);
+}
+
+// When the user presses the mousepad
+function mousePressed() {
+    InterstellarObjects.push(new InterstellarObject(2, mouseX, mouseY))
+}
+
+
+function draw_sun() {
+    fill(color('#fff236'));
+    noStroke();
+    ellipse(window.innerWidth / 2, window.innerHeight / 2, 80, 80);
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+    background("#0c1f36");
+    draw_sun();
+
+    for (let i = 0; i < InterstellarObjects.length; i++) {
+        InterstellarObjects[i].display();
     }
-    ellipse(mouseX, mouseY, 80, 80);
+
 }
